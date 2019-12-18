b08f698cbbab66dab15a40e8ede8172dca88b8b7
Aleff <aleffbruno@gmail.com>
2019-12-17 22:01:22 -0300

    finalize simple rest api

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::get('/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::post('/tasks', 'TaskController@store')->name('tasks.store');
+
+Route::get('/tasks/{task}', 'TaskController@show')->name('tasks.show');
+
+Route::put('/tasks/{task}', 'TaskController@update')->name('tasks.update');
+
+Route::delete('/tasks/{task}', 'TaskController@destroy')->name('tasks.destroy');
+
+Route::get('/', function () {
+    return view('welcome');
+});
